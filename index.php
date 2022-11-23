f6dc078adf135a356c058a61714a0162c4baa44e
forticas <houssem@forticas.com>
2022-11-23 13:03:12 +0100

    update index

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ require_once  'src/controllers/UserController.php';
 // monsite.fr/index.php?controller=task&action=index
 
 
-$controller_name = ucfirst($_GET['controller']).'Controller';
-$action_name = $_GET['action'];
+$controller_name = ucfirst(strtolower($_GET['controller'])).'Controller';
+$action_name = strtolower($_GET['action']);
 
 
 $controller_instance = new $controller_name();
