80a38b8318909dd8a2001ea343c0994a700cfe2e
forticas <houssem@forticas.com>
2022-11-23 12:09:07 +0100

    Init Router

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
 
+require_once  'controllers/TaskController.php';
+require_once  'controllers/UserController.php';
+
+
+$controller_name = ucfirst($_GET['controller']).'Controller';
+$action_name = $_GET['action'];
+
+
+$controller_instance = new $controller_name();
+
+$controller_instance->$action_name();
