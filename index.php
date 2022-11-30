900275138c0c190c41e9611b4f30718b453c7257
forticas <houssem@forticas.com>
2022-11-30 10:30:54 +0100

    add filp/whoops error handler

--- index.php
+++ index.php
@@ -6,6 +6,11 @@ const BASE_DIR = '/to_do_poo';
 require_once 'vendor/autoload.php';
 require_once 'config/routes.php';
 
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+
 $request_uri = str_replace(BASE_DIR, '', $_SERVER['REQUEST_URI']);
 
 try {
